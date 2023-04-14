4d90a687a5286f04be2aad31f490bd71a904f1d8
TlpAyn <talipayan6035@gmail.com>
2023-04-14 22:01:23 +0200

    bos kalmasin devam geleccek

---
+++ src/test/java/XML/Deneme2.java
@@ -0,0 +1,43 @@
+package XML;
+
+public class Deneme2
+{
+    public static void main(String[] args) {
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+        System.out.println("asd");
+    }
+}
